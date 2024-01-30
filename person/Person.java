26a53ef83d1e2d5bd6d98573cdc710f0ea2fabd9
Rasthy <156677015+rasthydian@users.noreply.github.com>
2024-01-30 13:28:28 +0700

    Add files via upload

---
+++ person/Person.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.person;
+
+/**
+ *
+ * @author Rasthy
+ */
+public class Person {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
